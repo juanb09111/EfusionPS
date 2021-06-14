4c69ae8772e51a1b76cde7f7c836e1525b4a94de
Juan Pablo <lagos@narvi.cc.tut.fi>
2021-06-14 12:04:28 +0300

    PYN-30 update

--- models.py
+++ models.py
@@ -1,10 +1,12 @@
 from models_bank import mask_rcnn
 from models_bank.efficient_ps import EfficientPS
+from models_bank.efficient_ps_no_instance import EfficientPS as EfficientPS_no_instance
 import os.path
 import json
 import constants
 from models_bank.fuseblock_2d_3d import FuseNet
 from models_bank.efusion_ps import EfusionPS
+from models_bank.efusion_ps_no_instance import EfusionPS as EfusionPS_no_instance
 import config_kitti
 
 
@@ -20,6 +22,23 @@ def get_model_by_name(model_name):
                          min_size=config_kitti.MIN_SIZE,
                          max_size=config_kitti.MAX_SIZE,
                          n_number=config_kitti.N_NUMBER)
+    if model_name is "EfusionPS_no_instance":
+        return EfusionPS_no_instance(config_kitti.K_NUMBER,
+                         config_kitti.NUM_THING_CLASSES,
+                         config_kitti.NUM_STUFF_CLASSES,
+                         config_kitti.CROP_OUTPUT_SIZE,
+                         min_size=config_kitti.MIN_SIZE,
+                         max_size=config_kitti.MAX_SIZE,
+                         n_number=config_kitti.N_NUMBER)
+
+    if model_name is "EfficientPS_no_instance":
+        return EfficientPS_no_instance(config_kitti.BACKBONE,
+                           config_kitti.BACKBONE_OUT_CHANNELS,
+                           config_kitti.NUM_THING_CLASSES,
+                           config_kitti.NUM_STUFF_CLASSES,
+                           config_kitti.CROP_OUTPUT_SIZE,
+                           config_kitti.MIN_SIZE,
+                           config_kitti.MAX_SIZE)
 
     if model_name is "EfficientPS":
         return EfficientPS(config_kitti.BACKBONE,
